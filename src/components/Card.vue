88e3a116001f7b28af34657560d2aafb4bf32295
Roberto Andrade <srandradejr@gmail.com>
2022-02-19 19:50:45 +0000

    basic card functionality

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,19 +1,22 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-defineProps<{ 
-  msg: string
-  movie: Movie
-   }>()
-
-const count = ref(0)
+import store from '../store'
+const props = defineProps<{ 
+  movie: Movie;
+   }>();
 </script>
 
 <template>
-  <h1>{{movie.title}}</h1>
+  <h1 v-html="movie.title"></h1>
+  <h2>{{movie.id}}</h2>
   <img :src="movie.img" :alt="movie.imgAlt">
+  <p>{{movie.shortDescription}}</p>
+  <button @click="store.toggleFavorite(props.movie.id)">{{movie.favorited ? 'unfavorite' : 'favorite'}}</button>
 </template>
 
 <style scoped>
+img {
+  max-width: 100%;
+}
 a {
   color: #42b983;
 }
