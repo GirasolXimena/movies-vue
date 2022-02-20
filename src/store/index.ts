b20353c51358935a117699e1067243743fc082e1
Roberto Andrade <srandradejr@gmail.com>
2022-02-20 04:00:03 +0000

    added favorites array

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,19 +3,17 @@ import data from "../api";
 
 const store = reactive({
   movies: data.movies,
+  favorites: [] as Movie[],
   toggleFavorite(id: number) {
     const movie = this.movies.find(movie => movie.id === id);
-    if (movie) {
-      movie.favorited = !movie.favorited;
+    if (movie && !movie.favorited) {
+      movie.favorited = true;
+      this.favorites.push(movie);
+    } else if (movie && movie.favorited) {
+      movie.favorited = false;
+      this.favorites = this.favorites.filter(fav => fav.id !== id);
     }
-  },
-  increment() {
-    this.count++
-  },
-  decrement() {
-    this.count--
-  },
-  count: 0
+  }
 })
 
 export default store
