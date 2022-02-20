548a97d4083717c199a97de4b2c9770176d14822
Roberto Andrade <srandradejr@gmail.com>
2022-02-20 04:08:58 +0000

    compact favorites list

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,7 +10,13 @@ import FavoritesList from '../components/FavoritesList.vue';
   <div class="gallery">
     <Card class="card" v-for="movie in store.movies" :key="movie.id" :movie="movie" />
   </div>
-  <FavoritesList />
+  <ol>
+    <li class="favorites" v-for="movie in store.favorites">
+    <button @click="store.toggleFavorite(movie.id)" >&lt;/3</button>
+      <h5>{{movie.title}}</h5>
+      <p>{{movie.shortDescription}}</p>
+    </li>
+  </ol>
 </div>
 </template>
 
@@ -19,6 +25,11 @@ import FavoritesList from '../components/FavoritesList.vue';
   display: flex;
   flex-wrap: wrap;
   justify-content: space-around;
+}
+.favorites {
+  margin: 2em 1em;
+  display: flex;
+  align-items: baseline;
 }
 a {
   color: #42b983;
